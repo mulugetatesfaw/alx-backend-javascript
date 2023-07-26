d249b2c7aa473827ce6bd0ad659d11c725436283
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-07-25 23:51:18 -0700

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,3 @@
-export default function getResponseFromAPI(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject(new Error(`Error retrieving data from API: ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => {
-      reject(new Error('Network error occurred'));
-    };
-    xhr.send();
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
