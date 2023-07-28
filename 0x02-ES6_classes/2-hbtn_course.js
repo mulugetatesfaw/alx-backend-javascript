50dc62bcbc3612f8e78365d1b9a713f2d3bb76e2
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-07-28 05:47:07 -0700

    Implement a class named HolbertonCourse:

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,28 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Getter and setter for name
   get name() {
     return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
   }
 
   set name(name) {
@@ -17,11 +32,6 @@ class HolbertonCourse {
     this._name = name;
   }
 
-  // Getter and setter for length
-  get length() {
-    return this._length;
-  }
-
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
@@ -29,13 +39,8 @@ class HolbertonCourse {
     this._length = length;
   }
 
-  // Getter and setter for students
-  get students() {
-    return this._students;
-  }
-
   set students(students) {
-    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
